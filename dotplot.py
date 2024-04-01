f91bc22a2c8ab91f449ca1478cf555a36989c3b2
kimzz14 <kimzz1004@gmail.com>
2024-04-01 12:30:13 +0000

    init

---
+++ dotplot.py
@@ -0,0 +1,219 @@
+from lib.KJH_SVG.KJH_SVG import element
+
+
+def get_color(identity):
+    if 99 < identity:
+        return 'black'
+    elif 90 < identity:
+        return 'red'
+    else:
+        return 'green'
+
+
+
+
+
+infile = 'test.blastn_m6'
+threshold_identity = 99
+threshold_length = 1000
+
+imageLength = 800
+margin = 80
+
+
+
+length_DICT = {}
+fin = open(infile)
+for line in fin:
+    query, sbjct, identity, alignment_length, mismatches, gap_openings, query_start, query_end, sbjct_start, sbjct_end, e_value, bit_score = line.rstrip('\n').split('\t')
+
+    identity = float(identity)
+    if identity < threshold_identity: continue
+
+    alignment_length = int(alignment_length)
+    if alignment_length < threshold_length: continue
+
+    query_start = int(query_start)
+    query_end = int(query_end)
+    sbjct_start = int(sbjct_start)
+    sbjct_end = int(sbjct_end)
+    
+    if not query in length_DICT: length_DICT[query] = [min(query_start, query_end), max(query_start, query_end)]
+    if not sbjct in length_DICT: length_DICT[sbjct] = [min(sbjct_start, sbjct_end), max(sbjct_start, sbjct_end)]
+
+    length_DICT[query][0] = min([length_DICT[query][0], query_start, query_end])
+    length_DICT[query][1] = max([length_DICT[query][1], query_start, query_end])
+
+    length_DICT[sbjct][0] = min([length_DICT[sbjct][0], sbjct_start, sbjct_end])
+    length_DICT[sbjct][1] = max([length_DICT[sbjct][1], sbjct_start, sbjct_end])
+fin.close()
+
+#for seqName, [min, max] in length_DICT.items():
+#    print(seqName, min, max)
+
+
+
+
+
+
+fin = open(infile)
+image_DICT = {}
+ratio_DICT = {}
+for line in fin:
+    query, sbjct, identity, alignment_length, mismatches, gap_openings, query_start, query_end, sbjct_start, sbjct_end, e_value, bit_score = line.rstrip('\n').split('\t')
+
+    identity = float(identity)
+    if identity < threshold_identity: continue
+
+    alignment_length = int(alignment_length)
+    if alignment_length < threshold_length: continue
+
+    query_start = int(query_start)
+    query_end = int(query_end)
+    sbjct_start = int(sbjct_start)
+    sbjct_end = int(sbjct_end)
+
+    key = (query, sbjct)
+
+    if not key in image_DICT: 
+        svg = element('svg', None)
+
+        svg.attr('viewBox', '0 0 ' + str(imageLength) + ' ' + str(imageLength))
+        svg.attr('height', imageLength)
+        svg.attr('width', imageLength)
+        svg.style('background', 'white')
+
+
+        
+
+
+
+        image_DICT[key] = svg
+        ratio_DICT[key] = {}
+
+        queryMinPos, queryMaxPos = length_DICT[query]
+        sbjctMinPos, sbjctMaxPos = length_DICT[sbjct]
+
+        queryLength = queryMaxPos - queryMinPos + 1
+        sbjctLength = sbjctMaxPos - sbjctMinPos + 1
+
+        ratio_DICT[key][query] = float(imageLength - margin * 2) / max([queryLength, sbjctLength])
+        ratio_DICT[key][sbjct] = float(imageLength - margin * 2) / max([queryLength, sbjctLength])
+        
+
+        #query
+        x1 = margin
+        x2 = margin + ratio_DICT[key][query] * queryLength
+        y1 = imageLength - margin
+        y2 = imageLength - margin
+        line = element('line', svg)
+        line.attr('x1', x1)
+        line.attr('y1', y1)
+        line.attr('x2', x2)
+        line.attr('y2', y2)
+        line.attr('stroke', 'black')
+        line.attr('stroke-width', '1')
+
+        x = margin
+        y = imageLength - margin + 15
+        text = element('text', svg)
+        text.attr('x', x)
+        text.attr('y', y)
+        text.attr('font-size', '10')
+        text.attr('fill', 'red')
+        text.add(queryMinPos)
+
+        x = margin + ratio_DICT[key][query] * queryLength  - 15
+        y = imageLength - margin + 15
+        text = element('text', svg)
+        text.attr('x', x)
+        text.attr('y', y)
+        text.attr('font-size', '10')
+        text.attr('fill', 'red')
+        text.add(queryMaxPos)
+
+        x = (margin * 2 + ratio_DICT[key][query] * queryLength  - 15)/2
+        y = imageLength - margin + 30
+        text = element('text', svg)
+        text.attr('x', x)
+        text.attr('y', y)
+        text.attr('font-size', '10')
+        text.attr('fill', 'red')
+        text.add(query)
+
+        #sbjct
+        y1 = imageLength - margin
+        y2 = imageLength - margin - ratio_DICT[key][sbjct] * sbjctLength
+        x1 = margin
+        x2 = margin
+        line = element('line', svg)
+        line.attr('x1', x1)
+        line.attr('y1', y1)
+        line.attr('x2', x2)
+        line.attr('y2', y2)
+        line.attr('stroke', 'black')
+        line.attr('stroke-width', '1')
+
+
+        x = margin - 50
+        y = imageLength - margin
+        text = element('text', svg)
+        text.attr('x', x)
+        text.attr('y', y)
+        text.attr('font-size', '10')
+        text.attr('fill', 'red')
+        text.add(sbjctMinPos)
+        print(x, y)
+
+
+        x = margin - 50
+        y = imageLength - margin - ratio_DICT[key][sbjct] * sbjctLength
+        text = element('text', svg)
+        text.attr('x', x)
+        text.attr('y', y)
+        text.attr('font-size', '10')
+        text.attr('fill', 'red')
+        text.add(sbjctMaxPos)
+        print(x, y)
+
+        x = margin - 60
+        y = (imageLength * 2 - margin * 2 - ratio_DICT[key][sbjct] * sbjctLength) /2
+        text = element('text', svg)
+        text.attr('x', x)
+        text.attr('y', y)
+        text.attr('font-size', '10')
+        text.attr('fill', 'red')
+        text.add(sbjct)
+        
+
+        
+
+
+
+    
+    x1 = margin + ratio_DICT[key][query] * (query_start - length_DICT[query][0])
+    y1 = imageLength - margin - ratio_DICT[key][sbjct] * (sbjct_start - length_DICT[sbjct][0])
+
+    x2 = margin + ratio_DICT[key][query] * (query_end - length_DICT[query][0])
+    y2 = imageLength - margin - ratio_DICT[key][sbjct] * (sbjct_end - length_DICT[sbjct][0])
+
+    
+    line = element('line', image_DICT[key])
+    line.attr('x1', x1)
+    line.attr('y1', y1)
+    line.attr('x2', x2)
+    line.attr('y2', y2)
+    line.attr('stroke', get_color(identity))
+    line.attr('stroke-width', '1')
+
+    line.attr('onmouseover', 'console.log([' + str(query_start) + ',' + str(query_end) + ',' + str(sbjct_start) + ',' + str(sbjct_end) + ']' + ')')
+
+
+for key, image in image_DICT.items():
+    fout = open('-'.join(key) + ".html", 'w')
+    fout.write(str(image))
+    fout.close()
+
+
+
+html = element('html', None)
